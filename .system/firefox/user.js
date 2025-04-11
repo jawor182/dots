2cdd0d4c5d620905ba188f284b2bf27d4c480249
jawor182 <kubajaworski59@outlook.com>
2025-04-11 19:19:07 +0200

    a

--- .system/firefox/user.js
+++
@@ -1,7 +0,0 @@
-user_pref("browser.tabs.inTitlebar", 1); 
-user_pref("browser.fullscreen.autohide", false);
-user_pref("ui.prefersReducedMotion", 1);
-user_pref("browser.tabs.loadBookmarksInBackground", true);  
-user_pref("browser.tabs.loadDivertedInBackground", true);  
-user_pref("browser.tabs.loadInBackground", true);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
