50ff2f96b0db1af93ce3f6ca9b6db26f59f16365
jawor182 <kubajaworski59@outlook.com>
2025-04-20 21:52:03 +0200

    Catppuccin

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,51 +1,100 @@
--- function ColorMyPencils(color)
---     color = color or "gruvbox"
---     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
+function ColorMyPencils(color)
+    color = color or "gruvbox"
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
 return {
-
-    -- {
-    --     "erikbackman/brightburn.vim",
-    -- },
-
     {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
         config = function()
-            require("gruvbox").setup({
-                terminal_colors = false, -- add neovim terminal colors
-                undercurl = true,
-                underline = true,
-                bold = true,
-                -- italic = {
-                --     strings = false,
-                --     emphasis = false,
-                --     comments = true,
-                --     operators = false,
-                --     folds = false,
-                -- },
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = true,
-                    folds = true,
+            require("catppuccin").setup({
+                flavor = "mocha", -- latte, frappe, macchiato, mocha
+                background = {
+                    dark = "mocha",
                 },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = false, -- invert background for search, diffs, statuslines and errors
-                contrast = "hard", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
+                transparent_background = false,
+                show_end_of_buffer = false,
+                term_colors = true,
+                dim_inactive = {
+                    enabled = false,
+                    shade = "dark",
+                    percentage = 0.15,
+                },
+                no_italic = true,
+                no_bold = false,
+                no_underline = false,
+                styles = {
+                    comments = { "italic" },
+                    conditionals = { "italic" },
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                    operators = {},
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = false,
+                        indentscope_color = "",
+                    },
+                },
             })
-            vim.cmd("colorscheme gruvbox")
+
+            vim.cmd("colorscheme catppuccin")
         end,
     },
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     name = "gruvbox",
+    --     config = function()
+    --         require("gruvbox").setup({
+    --             terminal_colors = true, -- add neovim terminal colors
+    --             undercurl = true,
+    --             underline = true,
+    --             bold = true,
+    --             -- italic = {
+    --             --     strings = false,
+    --             --     emphasis = false,
+    --             --     comments = true,
+    --             --     operators = false,
+    --             --     folds = false,
+    --             -- },
+    --             italic = {
+    --                 strings = true,
+    --                 emphasis = true,
+    --                 comments = true,
+    --                 operators = true,
+    --                 folds = true,
+    --             },
+    --             strikethrough = true,
+    --             invert_selection = false,
+    --             invert_signs = false,
+    --             invert_tabline = false,
+    --             invert_intend_guides = false,
+    --             inverse = false,   -- invert background for search, diffs, statuslines and errors
+    --             contrast = "dark", -- can be "hard", "soft" or empty string
+    --             palette_overrides = {},
+    --             overrides = {},
+    --             dim_inactive = false,
+    --             transparent_mode = false,
+    --         })
+    --         vim.cmd("colorscheme gruvbox")
+    --     end,
+    -- },
 }
