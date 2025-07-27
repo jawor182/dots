816ce08e13e9c3e73f2f1e23bee7981a97113ce2
jawor182 <kubajaworski59@outlook.com>
2025-07-27 16:12:10 +0200

    Removal of wayland. Time for stability

--- .config/yazi/init.lua
+++
@@ -1,8 +0,0 @@
----@diagnostic disable: undefined-global
-require("full-border"):setup({
-    -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-    type = ui.Border.ROUNDED,
-})
--- function Status:size()
--- end
-
