d2e1dcf7b2ef94d92df0af6fc8828189768381fd
jawor182 <kubajaworski59@outlook.com>
2025-08-26 19:42:40 +0200

    Better keymaps for telescope

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -56,7 +56,6 @@ vim.keymap.set("n", "<leader>wT", require("neowiki").open_wiki_new_tab, { desc =
 
 -- vim.keymap.set("n","<leader>f",":Pick files<CR>", {desc = "Open Pick"})
 
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
-vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
-vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
+vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files, {})
+vim.keymap.set("n", "<leader>gr", require("telescope.builtin").live_grep, {})
+vim.keymap.set("n", "<leader>h", require("telescope.builtin").help_tags, {})
