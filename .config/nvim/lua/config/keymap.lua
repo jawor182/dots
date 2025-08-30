29055edbf4bfc21869cfdd9f60736cbf28cf0ebd
jawor182 <kubajaworski59@outlook.com>
2025-08-30 19:30:27 +0200

    Yazi.nvim is back

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,7 +1,7 @@
 
 
 vim.g.mapleader = " "
-vim.keymap.set("n",'<leader>tr',vim.cmd.Ex)
+vim.keymap.set("n",'<leader>tr',vim.cmd.Yazi)
 vim.keymap.set("n",'K', vim.lsp.buf.hover, {})
 vim.keymap.set("n",'<leader>m',vim.cmd.Mason)
 vim.keymap.set("n",'<leader>l',vim.cmd.Lazy)
