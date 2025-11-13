175afcbd134181e87b605491d11daf9b19a2240a
jawor <jakubjaworski@mailbox.org>
2025-11-13 20:46:23 +0100

    Simplyfying swayidle

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -11,11 +11,5 @@ xrdb -m "$XRESOURCES" && echo "Xft.dpi:96" | xrdb -m
 sleep 0.5 && wallpaper random &
 
 swayidle -w \
-    timeout 180 'brightnessctl -sd rgb:kbd_backlight set 0' \
-        resume 'brightnessctl -rd rgb:kbd_backlight' \
-    timeout 180 'loginctl lock-session && gtklock' \
-    timeout 600 'hyprctl dispatch dpms off' \
-        resume 'hyprctl dispatch dpms on && brightnessctl -r' \
-    timeout 3600 'systemctl suspend' \
-    before-sleep 'loginctl lock-session && gtklock' \
-    after-resume 'hyprctl dispatch dpms on'
+    timeout 500 "wlr-dpms off && gtklock &" \
+    resume "wlr-dpms on"
