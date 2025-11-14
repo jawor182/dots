f1504ea03cbbf8dfc2618a4fe775e0e7df13c8f3
jawor <jakubjaworski@mailbox.org>
2025-11-14 13:35:38 +0100

    Tweaks to foot, mango, packages list and backup script

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -6,10 +6,17 @@ foot -s &
 dunst &
 mpd-mpris &
 playerctld &
-screen &
 xrdb -m "$XRESOURCES" && echo "Xft.dpi:96" | xrdb -m
-sleep 0.5 && wallpaper random &
+wallpaper random &
 
 swayidle -w \
-    timeout 500 "wlr-dpms off && gtklock &" \
-    resume "wlr-dpms on"
+    timeout 180 'brightnessctl -sd rgb:kbd_backlight set 0' \
+        resume 'brightnessctl -rd rgb:kbd_backlight' \
+    timeout 180 'loginctl lock-session & gtklock &' \
+    timeout 600 'wlr-dpms off' \
+        resume 'wlr-dpms on && brightnessctl -r' \
+    timeout 3600 'systemctl suspend' \
+    lock 'pidof gtklock || gtklock' \
+    before-sleep 'loginctl lock-session' \
+    after-resume 'wlr-dpms on'
+
