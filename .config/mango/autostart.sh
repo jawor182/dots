86f4814cc1164b45122d4427f76f50e8a754688a
jawor <jakubjaworski@mailbox.org>
2025-11-14 22:20:37 +0100

    Another tweaks to mango and workaround to the gtklock bug by changing the cursor placement via focusmon

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+host=$(cat /etc/hostname)
+
 waybar &
 swww-daemon &
 foot -s &
@@ -9,14 +11,16 @@ playerctld &
 xrdb -m "$XRESOURCES" && echo "Xft.dpi:96" | xrdb -m
 wallpaper random &
 
+[ "$host" = "desktop" ] && monitor="DP-1" || monitor="eDP-1"
+
 swayidle -w \
-    timeout 180 'brightnessctl -sd rgb:kbd_backlight set 0' \
-        resume 'brightnessctl -rd rgb:kbd_backlight' \
-    timeout 180 'loginctl lock-session & gtklock &' \
-    timeout 600 'wlr-dpms off' \
-        resume 'wlr-dpms on && brightnessctl -r' \
-    timeout 3600 'systemctl suspend' \
-    lock 'pidof gtklock || gtklock' \
-    before-sleep 'loginctl lock-session' \
-    after-resume 'wlr-dpms on'
+    timeout 180 "brightnessctl -sd rgb:kbd_backlight set 0" \
+        resume "brightnessctl -rd rgb:kbd_backlight" \
+    timeout 180 "loginctl lock-session & mmsg -d focusmon,$monitor & pidof gtklock || gtklock &" \
+    timeout 600 "wlr-dpms off" \
+        resume "wlr-dpms on && brightnessctl -r" \
+    timeout 3600 "systemctl suspend" \
+    lock "mmsg -d focusmon,$monitor & pidof gtklock || gtklock" \
+    before-sleep "loginctl lock-session" \
+    after-resume "wlr-dpms on"
 
